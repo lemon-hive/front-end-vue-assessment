da05417c5750ddc2ac27c5c5fb4e2ffb39ab5331
asifulhaque087 <asifulhaque087@gmail.com>
2022-09-02 12:45:53 +0600

    add progressbar

---
+++ src/components/CirCularProgress.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="skill" :style="cssProps">
+    <div class="outer">
+      <div class="inner">
+        <div id="number" ref="numb"></div>
+      </div>
+    </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+      <defs>
+        <linearGradient id="GradientColor">
+          <stop offset="0%" stop-color="#DA22FF" />
+          <stop offset="100%" stop-color="#9733EE" />
+        </linearGradient>
+      </defs>
+      <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarCom',
+  props: ['percent'],
+  components: {},
+  mounted() {
+    console.log('hello');
+
+    // const numb = document.querySelector('.numb');
+
+    const { numb } = this.$refs;
+
+    let counter = 0;
+    // console.log(Math.floor((2 / 3) * 100));
+
+    setInterval(() => {
+      if (counter === Math.floor((2 / 3) * 100)) {
+        clearInterval();
+      } else {
+        counter += 1;
+        numb.textContent = `${counter}%`;
+      }
+    }, 2000 / 66);
+  },
+
+  computed: {
+    cssProps() {
+      return {
+        '--hover-font-size': `${this.baseFontSize * 2}em`,
+        '--animation-per': `${200}`,
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.skill {
+  width: 160px;
+  height: 160px;
+  /*    background-color: cornflowerblue;*/
+  position: relative;
+}
+
+.outer {
+  height: 160px;
+  width: 160px;
+  border-radius: 50%;
+  /*  border: 2px solid orangered;*/
+  padding: 20px;
+  box-shadow: 6px 6px 10px -1px rgba(0, 0, 0, 0.15), -6px -6px 10px -1px rgba(255, 255, 255, 0.7);
+}
+
+.inner {
+  height: 120px;
+  width: 120px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /*  border: 2px solid orangered*/
+  box-shadow: inset 4px 4px 6px -1px rgba(0, 0, 0, 0.2),
+    inset -4px -4px 6px -1px rgba(255, 255, 255, 0.7), -0.5px -0.5px 0px rgba(255, 255, 255, 1),
+    0.5px 0.5px 0px rgba(0, 0, 0, 0.15), 0px 12px 10px -10px rgba(0, 0, 0, 0.05);
+}
+
+#number {
+  font-weight: 600;
+  color: #555;
+}
+
+circle {
+  fill: none;
+  stroke: url(#GradientColor);
+  stroke-width: 20px;
+  stroke-dasharray: 450;
+  stroke-dashoffset: 450;
+  /* stroke-dashoffset: 50; */
+  animation: anim 2s linear forwards;
+  /* transition: all 1s; */
+}
+
+@keyframes anim {
+  100% {
+    /* stroke-dashoffset: 157.5; */
+    stroke-dashoffset: var(--animation-per);
+  }
+}
+
+/*Put it on the top of our circle*/
+svg {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
